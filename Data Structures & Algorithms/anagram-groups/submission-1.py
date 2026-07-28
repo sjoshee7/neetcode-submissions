ae703de1b0ee7cbcc4049b43601a6c659bf48826
sjoshee7 <samyog.joshee@gmail.com>
2026-07-28 09:01:52 -0600

    Delete Data Structures & Algorithms/anagram-groups/submission-1.py

--- Data Structures & Algorithms/anagram-groups/submission-1.py
+++
@@ -1,9 +0,0 @@
-class Solution:
-    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
-        anagrams = {}
-        for string in strs:
-            key = ''.join(sorted(string))
-            if key not in anagrams:
-                anagrams[key] = []
-            anagrams[key].append(string)
-        return list(anagrams.values())
